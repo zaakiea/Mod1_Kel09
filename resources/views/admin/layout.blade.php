94de787ff9bbbffb89df528ec61cbcf5e43382d5
TunaTerbang <dzaki1atmaja@gmail.com>
2026-02-21 12:48:21 +0700

    Update resources/views/admin/layout/t.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Modul 1 Kelompok XX</title>
+    <title>Modul 1 Kelompok 09</title>
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Styles -->
@@ -18,7 +18,7 @@
 <body class="antialiased">
     <nav class="navbar bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('admin.index') }}">Modul 1 SBD - Kelompok XX</a>
+            <a class="navbar-brand" href="{{ route('admin.index') }}">Modul 1 SBD - Kelompok 09</a>
         </div>
     </nav>
     <div class="container"> @yield('content')
